09daf773f82969ef63dddb4dcb8cf44e548f7780
Muhammad Ikram <ikram2500@gmail.com>
2026-02-09 16:36:41 +0500

    some issue resolve

---
+++ multi_doc_chat/logger/__init__.py
@@ -0,0 +1,9 @@
+
+from multi_doc_chat.logger.custom_logger import CustomLogger as _CustomLogger  # backward compat
+try:
+    from .custom_logger import CustomLogger
+except Exception:
+    CustomLogger = _CustomLogger
+
+# Expose a global structlog-style logger used across the codebase
+GLOBAL_LOGGER = CustomLogger().get_logger(__name__)
